888ea6f6437db5a9d3ab53dc4861172c44bd78b0
Matteo Mirisola <mirimmm94@gmail.com>
2019-09-16 11:48:53 +0200

    .

--- src/screens/InfoScreen.js
+++ src/screens/InfoScreen.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { observer, inject } from 'mobx-react';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { observer, inject } from "mobx-react";
 
-@inject('store') @observer
+@inject("store")
+@observer
 export default class InfoScreen extends React.Component {
-
   static navigationOptions = {
-    title: 'Info',
+    title: "Info"
   };
 
   showDetails = () => {
     return (
-      <View style={{ flex: 1, justifyContent: 'center' }}>
+      <View style={{ flex: 1, justifyContent: "center" }}>
         <Text style={styles.paragraph}>
           Model: {this.props.store.model} Country: {this.props.store.country}
         </Text>
@@ -21,7 +21,7 @@ export default class InfoScreen extends React.Component {
 
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center' }}>
+      <View style={{ flex: 1, justifyContent: "center" }}>
         {this.showDetails()}
       </View>
     );
@@ -32,6 +32,6 @@ const styles = StyleSheet.create({
   paragraph: {
     margin: 24,
     fontSize: 18,
-    textAlign: 'center',
-  },
+    textAlign: "center"
+  }
 });
