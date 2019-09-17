cd426b7df5c9d49f40e0df6e6f4e14f04fac7769
Matteo Mirisola <mirimmm94@gmail.com>
2019-09-17 12:00:10 +0200

    add mylistitem pure-component

---
+++ src/components/MyListItem.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Image, TouchableOpacity, StyleSheet } from "react-native";
+import { ListItem } from "react-native-elements";
+import { ICONS_CAR } from "../constantsCars";
+
+//To optimize cars list performance
+export default class MyListItem extends React.PureComponent {
+  //Cars png
+  getIconCars = (iconName) => {
+    let icon = ICONS_CAR[iconName];
+    //Default car icon if we haven't the corresponding png
+    if (!icon) {
+      iconName = "circle_grey";
+    }
+    return ICONS_CAR[iconName];
+  };
+
+  //Show cars list
+  render() {
+    let iconCar = this.getIconCars(this.props.icon);
+    return (
+      <TouchableOpacity>
+        <ListItem
+          title={this.props.title}
+          leftAvatar={
+            <Image source={iconCar} style={styles.images} />
+          }
+          bottomDivider
+          chevron
+        />
+      </TouchableOpacity>
+    );
+  }
+}
+
+//Style
+const styles = StyleSheet.create({
+  images: { width: 30, height: 30 }
+});
