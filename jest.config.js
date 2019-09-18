c1bbc97938546e4bef8f05aa5b29803b97ae3a3a
Matteo Mirisola <mirimmm94@gmail.com>
2019-09-18 13:16:43 +0200

    add app_test

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,7 @@
 module.exports = {
   preset: "jest-expo",
   modulePathIgnorePatterns: [".yarn",".npm"],
+  transformIgnorePatterns: [
+    "node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|sentry-expo|native-base)"
+  ]
 };
