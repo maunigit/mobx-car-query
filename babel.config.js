a3d4609e27a3cc25e7645355e77c931582de56e2
Matteo Mirisola <mirimmm94@gmail.com>
2019-09-18 17:34:38 +0200

    add plugin-proposal-decorators to fix class properties

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,15 @@
 module.exports = function(api) {
   api.cache(true);
   return {
+    plugins: [
+      ["@babel/plugin-proposal-decorators", { "legacy": true }],
+      ["@babel/plugin-proposal-class-properties", { "loose": true }]
+    ],
     presets: ['babel-preset-expo','@babel/preset-env', '@babel/preset-react'],
     env: {
       "production": {
         "plugins": ["transform-remove-console"]
       }
-    },
-    plugins: ["@babel/plugin-proposal-class-properties"]
+    }    
   };
 };
