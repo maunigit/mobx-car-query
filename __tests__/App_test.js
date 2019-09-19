0d7471021809f75cc708ad1360c3c0e5db9ef4fd
Matteo Mirisola <mirimmm94@gmail.com>
2019-09-19 11:41:23 +0200

    fix lint

--- __tests__/App_test.js
+++ __tests__/App_test.js
@@ -5,12 +5,12 @@ import App from '../App';
 
 //Unit test
 it('App renders without crashing', () => {
-    const rendered = renderer.create(<App />).toJSON();
-    expect(rendered).toBeTruthy();
-    });
+  const rendered = renderer.create(<App />).toJSON();
+  expect(rendered).toBeTruthy();
+});
 
-    //Snapshot test
-    it('App test against snapshot', () => {
-        const tree = renderer.create(<App />).toJSON();
-        expect(tree).toMatchSnapshot();
-        });
+//Snapshot test
+it('App test against snapshot', () => {
+  const tree = renderer.create(<App />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
