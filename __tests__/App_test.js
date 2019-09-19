001d4add93279d99e32dc3186d8ca6684dbbd381
Matteo Mirisola <mirimmm94@gmail.com>
2019-09-19 09:18:55 +0200

    first two test

--- __tests__/App_test.js
+++ __tests__/App_test.js
@@ -3,19 +3,14 @@ import renderer from 'react-test-renderer';
 
 import App from '../App';
 
-/*
+//Unit test
 it('App renders without crashing', () => {
     const rendered = renderer.create(<App />).toJSON();
     expect(rendered).toBeTruthy();
     });
 
+    //Snapshot test
     it('App test against snapshot', () => {
         const tree = renderer.create(<App />).toJSON();
         expect(tree).toMatchSnapshot();
-        });*/
-
-describe('<App />', () => {
-  it('has 1 child', () => {const tree = renderer.create(<App />).toJSON();
-    expect(tree.children.length).toBe(1); 
-  });
-});
+        });
