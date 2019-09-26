c7bea467d727f531c0f1be36648024c33f9200be
Matteo Mirisola <mirimmm94@gmail.com>
2019-09-26 18:56:09 +0200

    add years

--- src/stores/Store.js
+++ src/stores/Store.js
@@ -4,6 +4,8 @@ import { observable } from 'mobx';
 class Store {
   @observable data = [];
   @observable make = '';
+  @observable maxYear = '';
+  @observable minYear = '';
 }
 
 export default new Store();
