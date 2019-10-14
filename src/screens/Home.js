6090685e012906f3f3d779bfe2bee68478ecae5a
Matteo Mirisola <mirimmm94@gmail.com>
2019-10-14 12:57:03 +0200

    adds filter for app logic

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, TouchableOpacity } from 'react-native';
 import { observer, inject } from 'mobx-react';
-import { Button } from 'react-native-paper';
+import { Button, List, TouchableRipple, Title } from 'react-native-paper';
 import globalStyles from '../styles/Style';
 
 @inject('store')
@@ -12,17 +12,35 @@ export default class Home extends React.Component {
     title: 'Home',
   };
 
-  //Show button to view Makes
+  //Show filters
   render() {
-    return (
-      <View style={globalStyles.homeContainer}>
+    return (<View style={globalStyles.container}>
+        <TouchableOpacity >
+          <List.Item button onPress={() => this.props.navigation.navigate('Make')} title={'Make'} />
+        </TouchableOpacity>
+        <TouchableOpacity >
+          <List.Item button onPress={() => this.props.navigation.navigate('Year')} title={'Year'} />
+        </TouchableOpacity>   
+        <TouchableOpacity >
+          <List.Item button onPress={() => this.props.navigation.navigate('Model')} title={'Model'} />
+        </TouchableOpacity>   
+        <TouchableOpacity >
+          <List.Item button onPress={() => this.props.navigation.navigate('Trim')} title={'Trim'} />
+        </TouchableOpacity>
+        <View style={globalStyles.homeContainer}>
+        <Button
+          color="orange"
+          mode="contained"
+          onPress={() => this.props.navigation.navigate('Detail')}>
+          show details (app logic)
+        </Button>
         <Button
           color="green"
           mode="contained"
           onPress={() => this.props.navigation.navigate('Make')}>
-          View Car
+          show makes (web logic)
         </Button>
-      </View>
-    );
+        </View>
+    </View>);
   }
 }
