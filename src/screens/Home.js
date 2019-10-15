237840a2186cb9f2c686f69903e68f3997f7f1ad
Matteo Mirisola <mirimmm94@gmail.com>
2019-10-15 11:43:23 +0200

    add icons and descriptions

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -14,27 +14,37 @@ export default class Home extends React.Component {
 
   //Show filters
   render() {
-    return (<View style={globalStyles.container}>
+    return (
+      <View style={globalStyles.homeContainer}>
         <TouchableOpacity >
-          <List.Item button onPress={() => this.props.navigation.navigate('Make')} title={'Make'} />
+          <List.Item button onPress={() => this.props.navigation.navigate('Year')} 
+            title={'Year'} description={'Select a year'}
+            left={props => <List.Icon {...props} icon="calendar-range"/>}/>
         </TouchableOpacity>
         <TouchableOpacity >
-          <List.Item button onPress={() => this.props.navigation.navigate('Year')} title={'Year'} />
+          <List.Item button onPress={() => this.props.navigation.navigate('Make')}
+            title={'Make'} description={'Select a make'}
+            left={props => <List.Icon {...props} icon="car"/>}/>
+        </TouchableOpacity>           
+        <TouchableOpacity >
+          <List.Item button onPress={() => this.props.navigation.navigate('Model')} 
+            title={'Model'} description={'Avaiable models of a make'} disabled={true} 
+            left={props => <List.Icon {...props} icon="car-side"/>}/>
         </TouchableOpacity>   
         <TouchableOpacity >
-          <List.Item button onPress={() => this.props.navigation.navigate('Model')} title={'Model'} />
-        </TouchableOpacity>   
-        <TouchableOpacity >
-          <List.Item button onPress={() => this.props.navigation.navigate('Trim')} title={'Trim'} />
+          <List.Item button onPress={() => this.props.navigation.navigate('Trim')} 
+            title={'Trim'} description={'Avaiable trims of a model'} disabled={true} 
+            left={props => <List.Icon {...props} icon="engine-outline"/>}/>
         </TouchableOpacity>
-        <View style={globalStyles.homeContainer}>
-        <Button
-          color="orange"
-          mode="contained"
-          onPress={() => this.props.navigation.navigate('Detail')}>
-          show details
-        </Button>
-        </View>
-    </View>);
+        <View style={globalStyles.button}>
+          <Button
+            color="orange"
+            mode="contained"
+            onPress={() => this.props.navigation.navigate('Detail')}>
+            show details
+          </Button>
+        </View>        
+      </View>    
+    );
   }
 }
