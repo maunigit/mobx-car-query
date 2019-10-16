7287d3ebc66cbde31e6faca68e77963f82abd9bd
Matteo Mirisola <mirimmm94@gmail.com>
2019-10-16 10:22:43 +0200

    rename and add items

--- src/stores/Store.js
+++ src/stores/Store.js
@@ -3,14 +3,16 @@ import { observable } from 'mobx';
 //Place to store information
 class Store {
   @observable makes = [];
-  @observable make = '';
+  @observable make_id = '';
+  @observable make_display = '';
   @observable maxYear = '';
   @observable minYear = '';
   @observable year = 0;
   @observable models = [];
-  @observable model = '';
+  @observable model_name = '';
   @observable trims = [];
-  @observable trim_model_id = '';
+  @observable trim_id = '';
+  @observable trim_name = '';
   @observable details = [];
 }
 
