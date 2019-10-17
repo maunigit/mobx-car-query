a95a7969b6c5d8b7cc224fed3c6f1a8a106214c1
Matteo Mirisola <mirimmm94@gmail.com>
2019-10-17 12:47:21 +0200

    implement enzyme wrappedComponent

--- __tests__/screens/Home_test.js
+++ __tests__/screens/Home_test.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
 import Home from '../../src/screens/Home';
 import Store from "../../src/stores/Store";
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 //see https://github.com/mobxjs/mobx-react#testing-store-injection
-//Snapshot test
+const store = Store; 
 configure({adapter: new Adapter()});
-it('Home renders correctly', () => {  
-  const store = Store;  
-  const wrapper = shallow(<Home store={store} />);
+
+//render with enzyme
+it('Home renders correctly', () => {     
+  const wrapper = shallow(<Home.wrappedComponent store={store}/>);
   expect(wrapper).toMatchSnapshot();
 });
+
+/*
+//render without enzyme
+it('Home renders correctly', () => {     
+  const tree = renderer.create(<Home store={store}/>).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+*/
