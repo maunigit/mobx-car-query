a6dbfa74dbc9ec648cdb1c3efa57697eb3cc19a0
Matteo Mirisola <mirimmm94@gmail.com>
2019-10-17 13:00:20 +0200

    .

--- __tests__/screens/Home_test.js
+++ __tests__/screens/Home_test.js
@@ -3,6 +3,7 @@ import Home from '../../src/screens/Home';
 import Store from "../../src/stores/Store";
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { List } from 'react-native-paper';
 
 //see https://github.com/mobxjs/mobx-react#testing-store-injection
 const store = Store; 
@@ -12,6 +13,12 @@ configure({adapter: new Adapter()});
 it('Home renders correctly', () => {     
   const wrapper = shallow(<Home.wrappedComponent store={store}/>);
   expect(wrapper).toMatchSnapshot();
+});
+
+it('Home renders X', () => {     
+  const wrapper = shallow(<Home.wrappedComponent store={store}/>);
+  const listItemElement = wrapper.find(List.Item);  
+  expect(listItemElement.props().disabled).toBe(false);
 });
 
 /*
