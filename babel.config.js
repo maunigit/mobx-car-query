5fb648e5a53c7d08284ea51730c71e2b9513997c
Matteo Mirisola <mirimmm94@gmail.com>
2019-10-22 10:44:54 +0200

    FIX remove jest preset @babel/preset-env in babel.config that don't allows to run the app

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,5 @@
 //For a detailed explanation see: https://github.com/facebook/react-native/issues/20588
+//Remove jest preset: '@babel/preset-env' see: https://github.com/expo/expo-cli/issues/276
 
 module.exports = function(api) {
   api.cache(true);
@@ -8,7 +9,7 @@ module.exports = function(api) {
       ["@babel/plugin-proposal-decorators", { "legacy": true }],
       ["@babel/plugin-proposal-class-properties", { "loose": true }]
     ],
-    presets: ['babel-preset-expo','@babel/preset-env', '@babel/preset-react'],
+    presets: ['babel-preset-expo', '@babel/preset-react'],
     env: {
       "production": {
         "plugins": ["transform-remove-console"]
